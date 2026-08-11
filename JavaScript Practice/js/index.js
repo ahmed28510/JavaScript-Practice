59040a2970496c8eef44fd4a79354a84c7e45536
Mohammad Ahmed <ahmedghufran2010@gmail.com>
2026-08-11 17:31:10 +0500

    Practice java Script

--- JavaScript Practice/js/index.js
+++ JavaScript Practice/js/index.js
@@ -1,3 +1,14 @@
+// Simple Arithmetic Operations
+let a = 10;
+let b = 5;
+console.log("a =", a,"& b =", b);
+console.log("Addition of a and b is:", "a + b =", a + b);
+console.log("Subtraction of a and b is:", "a - b =", a - b);
+console.log("Multiplication of a and b is:", "a * b =", a * b);
+console.log("Division of a and b is:", "a / b =", a / b);
+console.log("Modulus of a and b is:", "a % b =", a % b);
+
+// Simple Mathematical Operations looks like a calculator
 var number1 = + prompt("Enter the first number:");
 var operator = prompt("Enter the operator");
 var number2 = + prompt("Enter the second number:");
@@ -14,4 +25,5 @@ else if (operator === "*") {
 }
 else if (operator === "/") {
     output.innerHTML = number1 / number2;
-}
+} 
+var result = number1 + " " + operator + " " + number2 + " = " + output.innerHTML;   
