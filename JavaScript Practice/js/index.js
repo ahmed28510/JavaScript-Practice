e3372e89440b536e6c620b2c532016c149817d3c
Mohammad Ahmed <ahmedghufran2010@gmail.com>
2026-08-19 16:48:35 +0500

    Java Script Practice

--- JavaScript Practice/js/index.js
+++ JavaScript Practice/js/index.js
@@ -1,7 +1,7 @@
 // Simple Arithmetic Operations
 let a = 10;
 let b = 5;
-console.log("a =", a,"& b =", b);
+console.log("a =", a, "& b =", b);
 console.log("Addition of a and b is:", "a + b =", a + b);
 console.log("Subtraction of a and b is:", "a - b =", a - b);
 console.log("Multiplication of a and b is:", "a * b =", a * b);
@@ -11,7 +11,7 @@ console.log("Modulus of a and b is:", "a % b =", a % b);
 //Unary Operators
 var a = 8;
 var b = 6;
-console.log("a =", a,"& b =", b);
+console.log("a =", a, "& b =", b);
 console.log("Increment of a is:", "a++ =", ++a);
 console.log("Decrement of b is:", "b-- =", --b);
 
@@ -32,8 +32,8 @@ else if (operator === "*") {
 }
 else if (operator === "/") {
     output.innerHTML = number1 / number2;
-} 
-var result = number1 + " " + operator + " " + number2 + " = " + output.innerHTML; 
+}
+var result = number1 + " " + operator + " " + number2 + " = " + output.innerHTML;
 
 function submitAge() {
     var currentUserAge = Number(document.getElementById("age").value);
@@ -91,14 +91,14 @@ console.log(colors);
 
 var userinput3 = +prompt("Which position of index you want to delete from an array");
 var userinput4 = +prompt("How many element you want to delete from an array");
-colors.splice(userinput3 ,userinput4);
+colors.splice(userinput3, userinput4);
 console.log(colors);
 
 
-var array = ['This','Is','Batch','22']
+var array = ['This', 'Is', 'Batch', '22']
 array = console.log(array);
 
-var animals = ['dog','cat','donkey','zebra','lion'];
+var animals = ['dog', 'cat', 'donkey', 'zebra', 'lion'];
 animals = animals.sort();
 console.log(animals);
 
@@ -112,7 +112,7 @@ for (var i = 1; i <= tablelimt; i++) {
 
 
 
-var cleanestcities = ["Karachi", "Lahore","Peshwar","Hydrabad"]
+var cleanestcities = ["Karachi", "Lahore", "Peshwar", "Hydrabad"]
 var userprompt = prompt("Enter city name ");
 var match = false;
 
@@ -129,59 +129,59 @@ if (match == false) {
 
 
 var city = prompt('Enter city name');
-var initialChar = city.slice(0,1);
+var initialChar = city.slice(0, 1);
 var remainingChar = city.slice(1);
-console.log(initialChar.toLocaleUpperCase()+remainingChar.toLocaleLowerCase());
+console.log(initialChar.toLocaleUpperCase() + remainingChar.toLocaleLowerCase());
 
 
 var month = prompt('Enter month name');
-if(month.length > 3){
-    var shortform = month.slice(0,3);
+if (month.length > 3) {
+    var shortform = month.slice(0, 3);
 }
 console.log(shortform);
 
 
 var str = prompt("Enter some text");
 var numChar = str.length;
-for(var i = 0; i < numChar; i++){
-    if(str.slice(i,i+2) === "  "){
+for (var i = 0; i < numChar; i++) {
+    if (str.slice(i, i + 2) === "  ") {
         alert("No double spaces!");
         break;
-   }
+    }
 }
 
- function submit() {
-   var userInput = document.getElementById("textarea").value;
-   var output = document.getElementById("output");
-   var textIndex = userInput.indexOf("Israel");
+function submit() {
+    var userInput = document.getElementById("textarea").value;
+    var output = document.getElementById("output");
+    var textIndex = userInput.indexOf("Israel");
 
-  // 1 -> userInput.slice(0, 6) --> Enemie
-  // 2 -> userInput.slice(1, 7) --> nemie
+    // 1 -> userInput.slice(0, 6) --> Enemie
+    // 2 -> userInput.slice(1, 7) --> nemie
 
-  // 18 -> userInput.slice(18, 24) --> Israel
+    // 18 -> userInput.slice(18, 24) --> Israel
 
-       userInput = userInput.slice(0, textIndex) + "*******" + userInput.slice(i + 7);
-   for (var i = 0; i < userInput.length; i++) {
-     if (userInput.slice(i, i + 6) === "Israel") {
-       userInput = userInput.slice(0, i) + "*******" + userInput.slice(i + 7);
-     }
-   }
+    userInput = userInput.slice(0, textIndex) + "*******" + userInput.slice(textIndex + 7);
+    for (var i = 0; i < userInput.length; i++) {
+        if (userInput.slice(i, i + 6) === "Israel") {
+            userInput = userInput.slice(0, i) + "*******" + userInput.slice(i + 7);
+        }
+    }
 
-//    // Enemie of Pak are Israel and USA
-   output.innerHTML = userInput;
+    //    // Enemie of Pak are Israel and USA
+    output.innerHTML = userInput;
 
-  console.log(userInput);
+    console.log(userInput);
 }
 
-for (var i = 0; i < text.length; i++){
-  if (text.charAt(i)==="!"){
-    alert("Exclamation point found!");
-    break;
-  }
+for (var i = 0; i < text.length; i++) {
+    if (text.charAt(i) === "!") {
+        alert("Exclamation point found!");
+        break;
+    }
 }
 
-var userinput = prompt ('Enter your name');
-  var index = userinput.indexOf("Pakistan");
+var userinput = prompt('Enter your name');
+var index = userinput.indexOf("Pakistan");
 console.log(index);
 
 
@@ -197,22 +197,22 @@ console.log(string.charAt(3));
 
 
 var string3 = 'Hydrabad';
-console.log(string3.replace('Hydra','Islam'));
+console.log(string3.replace('Hydra', 'Islam'));
 
-var string4 ='we are the student of Batch#22 and attend modern web development and etocs class'
-console.log (string4.replaceAll("and","&"))
+var string4 = 'we are the student of Batch#22 and attend modern web development and etocs class'
+console.log(string4.replaceAll("and", "&"))
 
 //Dies rolling
 
-function roll(){
-  var output = document.getElementById('output');
-  var randomNumber = Math.random();
-  var roundOfNumber = Math.ceil(randomNumber * 6);
+function roll() {
+    var output = document.getElementById('output');
+    var randomNumber = Math.random();
+    var roundOfNumber = Math.ceil(randomNumber * 6);
 
-  output.innerHTML = roundOfNumber;
+    output.innerHTML = roundOfNumber;
 }
 
-var days = ['sun','mon','thes','wed','Thue','fri','sat'];
+var days = ['sun', 'mon', 'thes', 'wed', 'Thue', 'fri', 'sat'];
 var rightnow = new Date();
 var day = rightnow.getDay();
 var month = rightnow.getMonth();
@@ -222,35 +222,35 @@ var currentHours = rightnow.getHours();
 var currentMins = rightnow.getMinutes();
 var currentsecs = rightnow.getSeconds();
 
-console.log(day,'day');
-console.log(month,'month');
-console.log(dayofMonth,'dayofMonth');
-console.log(currentYear,'currentYear');
-console.log(currentHours,'currentHours');
-console.log(currentMins,'currentMins');
-console.log(currentsecs,'currentsecs');
- 
+console.log(day, 'day');
+console.log(month, 'month');
+console.log(dayofMonth, 'dayofMonth');
+console.log(currentYear, 'currentYear');
+console.log(currentHours, 'currentHours');
+console.log(currentMins, 'currentMins');
+console.log(currentsecs, 'currentsecs');
+
 // console.log(days[day])
 
 var ramadan = new Date("Februray 8,2027");
 var currentDate = new Date();
-var difference =(ramadan.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24);
+var difference = (ramadan.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24);
 console.log(difference);
 
 
 var month = rightnow.getMonth();
-console.log(month,'month');
+console.log(month, 'month');
 
 var month = rightnow.getMonth();
-var currentmonth = month2('Monday','Tuesday');
-if(month === currentmonth){
+var currentmonth = month2('Monday', 'Tuesday');
+if (month === currentmonth) {
 
 }
 
 var days = ['sun', 'mon', 'thes', 'wed', 'Thue', 'fri', 'sat'];
 
 var months = ['January', 'February', 'March', 'April', 'May', 'June',
-              'July', 'August', 'September', 'October', 'November', 'December'];
+    'July', 'August', 'September', 'October', 'November', 'December'];
 
 var currentDate = new Date();
 
@@ -281,5 +281,63 @@ if (currentHours > 11) {
 } else {
     console.log("AM");
 }
-
 console.log(currentHours, "currentHours");
+
+
+
+
+
+
+var base = +prompt('Enter a Base^2 Value')
+var perpendicular = +prompt('Enter a Perpendicular^2 Value')
+
+function calculateSqure(num) {
+    return num * num
+   
+}
+
+function calculateHypotenuse() {
+    var baseSqure = calculateSqure(base)
+    var perpendicularSqure = calculateSqure(perpendicular)
+    return baseSqure + perpendicularSqure
+}
+
+console.log(calculateHypotenuse())
+
+function checkPalindrome(word) {
+    return word.split('').reverse().join('')
+}
+
+console.log(checkPalindrome('hello'))
+
+function checkPalindrome(word) {
+    for (var i = 0; i < word.length / 2; i++) {
+        if (word[i] !== word[word.length -1 -i]) {
+            return false
+        }
+
+    }
+    return true
+}
+
+console.log(checkPalindrome('momds'))
+
+
+// Checking the longest word in a sentence using javascript or hand made function
+function checkLongestWord(sentence) {
+    var result;
+    var length;
+    var sentenceArray = sentence.split(" ");
+    var longestlength = 0;
+    for (var i = 0; i < sentenceArray.length; i++) {
+        if (sentenceArray[i].length > longestlength) {
+            longestlength = sentenceArray[i].length;
+            result = sentenceArray[i];
+        }
+    };
+    return result;
+}
+console.log(checkLongestWord('Web Development Course'))
+
+
+
